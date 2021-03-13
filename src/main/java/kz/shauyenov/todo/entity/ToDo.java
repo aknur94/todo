1077524c6f8145fcb51d3d8fe20536dcfeea6aed
Aknur Shauyenov <alnur.shauenov@gmail.com>
2021-03-13 16:01:02 +0600

    migration tools

--- src/main/java/kz/shauyenov/todo/entity/ToDo.java
+++ src/main/java/kz/shauyenov/todo/entity/ToDo.java
@@ -5,9 +5,11 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "ToDo")
 public class ToDo {
 
     @Id
---
+++ src/main/resources/db/migration/V01/V202103131558__create_todo_table.sql
@@ -0,0 +1,5 @@
+create table ToDo(
+  id bigint serial,
+  title varchar(256),
+  done bit
+);
